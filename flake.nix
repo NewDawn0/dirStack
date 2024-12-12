0072f8bc4a351e0604de0cb625263c9c2930d894
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-12 17:29:05 +0100

    fix shellHook

--- flake.nix
+++ flake.nix
@@ -19,17 +19,32 @@
       overlays.default =
         (final: prev: { dirStack = self.packages.${prev.system}.default; });
       packages = eachSystem (system:
-        let pkgs = mkPkgs system;
+        let
+          pkgs = mkPkgs system;
+          meta = pkgs:
+            with pkgs.lib; {
+              description = "A better pushd interface";
+              homepage = "https://github.com/NewDawn0/dirStack";
+              maintainers = with maintainers; [ NewDawn0 ];
+              license = licenses.mit;
+            };
+          pkg = pkgs:
+            pkgs.rustPlatform.buildRustPackage {
+              meta = (meta pkgs);
+              pname = "dirStack";
+              version = "1.0.0";
+              propagatedBuildInputs = with pkgs; [ fzf ];
+              cargoLock.lockFile = ./Cargo.lock;
+              src = pkgs.lib.cleanSource ./.;
+            };
         in {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "dirStack";
+          default = pkgs.stdenv.mkDerivation {
+            name = "dirStack-wrapped";
             version = "1.0.0";
-            propagatedBuildInputs = with pkgs; [ fzf ];
-            cargoLock.lockFile = ./Cargo.lock;
-            src = pkgs.lib.cleanSource ./.;
+            phases = [ "installPhase" ];
             installPhase = ''
               mkdir -p $out/bin $out/lib
-              cp target/release/dirStack $out/bin/
+              cp ${pkg pkgs}/bin/dirStack $out/bin/
               echo "#!/usr/bin/env bash" > $out/lib/SOURCE_ME.sh
               $out/bin/dirStack --init >> $out/lib/SOURCE_ME.sh
               chmod +x $out/lib/SOURCE_ME.sh
@@ -37,12 +52,6 @@
             shellHook = ''
               source $out/lib/SOURCE_ME.sh
             '';
-            meta = with pkgs.lib; {
-              description = "A better pushd interface";
-              homepage = "https://github.com/NewDawn0/dirStack";
-              maintainers = with maintainers; [ NewDawn0 ];
-              license = licenses.mit;
-            };
           };
         });
     };
