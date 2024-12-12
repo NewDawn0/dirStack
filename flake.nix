c029da3e137ab8041aa74045f01811ba698baa1b
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-12 17:08:22 +0100

    add shell hook

--- flake.nix
+++ flake.nix
@@ -27,10 +27,20 @@
             propagatedBuildInputs = with pkgs; [ fzf ];
             cargoLock.lockFile = ./Cargo.lock;
             src = pkgs.lib.cleanSource ./.;
+            installPhase = ''
+              mkdir -p $out/bin $out/lib
+              cp target/release/dirStack $out/bin/
+              echo "#!/usr/bin/env bash" > $out/lib/SOURCE_ME.sh
+              $out/bin/dirStack --init >> $out/lib/SOURCE_ME.sh
+              chmod +x $out/lib/SOURCE_ME.sh
+            '';
+            shellHook = ''
+              source $out/lib/SOURCE_ME.sh
+            '';
             meta = with pkgs.lib; {
               description = "A better pushd interface";
               homepage = "https://github.com/NewDawn0/dirStack";
-              maintainers = with maintainers; [ "NewDawn0" ];
+              maintainers = with maintainers; [ NewDawn0 ];
               license = licenses.mit;
             };
           };
