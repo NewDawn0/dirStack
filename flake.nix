982f84b0d5a5d336dbd7a76b2453d27e895c84aa
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 15:12:36 +0100

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -1,63 +1,52 @@
 {
   description = "A better pushd interface";
 
-  inputs = {
-    utils.url = "github:NewDawn0/nixUtils";
-    nixpkgs.url = "github:nixos/nixpkgs";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { self, utils, nixpkgs, ... }@inputs:
-    let
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ inputs.rust-overlay.overlays.default ];
-        };
-    in {
-      overlays.default = final: prev: {
-        dirStack = self.packages.${prev.system}.default;
-      };
-      packages = utils.lib.eachSystem { inherit mkPkgs; } (pkgs:
-        let
-          version = "1.0.0";
-          meta = {
-            description = "Fast directory navigation tool with a quicklist";
-            longDescription = ''
-              This utility allows you to change directories quickly using a user defined list of frequently used paths.
-              It reduces the time spent on navigation and enhances workflow efficiency.
-            '';
-            homepage = "https://github.com/NewDawn0/dirStack";
-            license = pkgs.lib.licenses.mit;
-            maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-            platforms = pkgs.lib.platforms.all;
-          };
-          basePkg = pkgs.rustPlatform.buildRustPackage {
-            inherit meta version;
-            pname = "dirStack";
-            src = ./.;
-            useFetchCargoVendor = true;
-            cargoHash = "sha256-Q0PqPWT31k8n4YboELd/OgX4FsdSUtWnSyofuEoADnU=";
-            propagatedBuildInputs = with pkgs; [ fzf ];
-          };
-        in {
-          default = pkgs.stdenvNoCC.mkDerivation {
-            inherit meta version;
-            pname = "dirStack-wrapped";
-            src = null;
-            dontUnpack = true;
-            dontBuild = true;
-            dontConfigure = true;
-            installPhase = ''
-              install -D ${basePkg}/bin/dirStack -t $out/bin
-              mkdir -p $out/lib
-              echo "#!/${pkgs.runtimeShell}" > $out/lib/SOURCE_ME.sh
-              $out/bin/dirStack --init >> $out/lib/SOURCE_ME.sh
-            '';
-            shellHook = ''
-              source $out/lib/SOURCE_ME.sh
-            '';
-          };
-        });
+  outputs = { self, utils, ... }@inputs: {
+    overlays.default = final: prev: {
+      dirStack = self.packages.${prev.system}.default;
     };
+    packages = utils.lib.eachSystem { } (pkgs:
+      let
+        version = "1.0.0";
+        meta = {
+          description = "Fast directory navigation tool with a quicklist";
+          longDescription = ''
+            This utility allows you to change directories quickly using a user defined list of frequently used paths.
+            It reduces the time spent on navigation and enhances workflow efficiency.
+          '';
+          homepage = "https://github.com/NewDawn0/dirStack";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
+        };
+        basePkg = pkgs.rustPlatform.buildRustPackage {
+          inherit meta version;
+          pname = "dirStack";
+          src = ./.;
+          useFetchCargoVendor = true;
+          cargoHash = "sha256-Q0PqPWT31k8n4YboELd/OgX4FsdSUtWnSyofuEoADnU=";
+          propagatedBuildInputs = with pkgs; [ fzf ];
+        };
+      in {
+        default = pkgs.stdenvNoCC.mkDerivation {
+          inherit meta version;
+          pname = "dirStack-wrapped";
+          src = null;
+          dontUnpack = true;
+          dontBuild = true;
+          dontConfigure = true;
+          installPhase = ''
+            install -D ${basePkg}/bin/dirStack -t $out/bin
+            mkdir -p $out/lib
+            echo "#!/${pkgs.runtimeShell}" > $out/lib/SOURCE_ME.sh
+            $out/bin/dirStack --init >> $out/lib/SOURCE_ME.sh
+          '';
+          shellHook = ''
+            source $out/lib/SOURCE_ME.sh
+          '';
+        };
+      });
+  };
 }
